cd8f96dc69530581a9990592d6803531bc29ba2c
Bhavesh Sharma <141301287+Bhaveshgitstar@users.noreply.github.com>
2024-07-23 18:41:09 +0530

    Add files via upload

---
+++ server/config/index.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+dotenv.config();
+exports.PORT = process.env.PORT || 4001;
+
+exports.dbSecretFields = ["__v", "password"];
+
+exports.LOCALHOST_CLIENT_URL = process.env.LOCALHOST_CLIENT_URL;
+exports.LOCALHOST_SERVER_URL = process.env.LOCALHOST_SERVER_URL;
+
+exports.SESSIONS_SECRET = process.env.SESSIONS_SECRET;
+
+exports.BASE_CLIENT_URL = exports.LOCALHOST_CLIENT_URL;
+exports.BASE_SERVER_URL = exports.LOCALHOST_SERVER_URL;
+
+exports.CORS_CONFIG = {
+  origin: exports.BASE_CLIENT_URL,
+  credentials: true,
+};
+
+exports.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+exports.GOOGLE_CALLBACK_URL = `${exports.BASE_SERVER_URL}/auth/google/callback`;
+
+exports.GOOGLE_SERVICE_ACCOUNT_KEY =
+  JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY) || undefined;
